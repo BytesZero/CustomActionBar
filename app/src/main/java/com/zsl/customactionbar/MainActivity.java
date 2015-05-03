89a54648ee2ae4ac45099d5b97b509a7cc028540
zsl <zhengsl@tongxinyiliao.com>
2015-05-03 13:49:31 +0800

    ActionBar 基本完成

--- app/src/main/java/com/zsl/customactionbar/MainActivity.java
+++ app/src/main/java/com/zsl/customactionbar/MainActivity.java
@@ -1,14 +1,30 @@
 package com.zsl.customactionbar;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    TextView tv_left,tv_title,tv_right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initActionBar();
+
+    }
+
+    private void initActionBar() {
+        tv_left= (TextView) findViewById(R.id.ui_myactionbar_tv_left);
+        tv_title= (TextView) findViewById(R.id.ui_myactionbar_tv_title);
+        tv_right= (TextView) findViewById(R.id.ui_myactionbar_tv_right);
+
+        tv_left.setText("返回");
+        tv_title.setText("标题");
+        tv_right.setText("完成");
+
     }
 }
